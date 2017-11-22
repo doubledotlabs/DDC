af6f6bba788de1c6ee72d721f138b3579309768c
TheAndroidMaster <18jafenn90@gmail.com>
2017-11-22 09:58:57 -0500

    added user-select and empty page links

--- js/methods.js
+++ js/methods.js
@@ -1,11 +1,11 @@
 function largeCardsMethod(app) {
-  return "<div class=\"largecard\" onclick=\"location.href = \'" + "\';\" " //TODO: add urls
+  return "<div class=\"largecard\" onclick=\"setPage(\'404\');\" " //TODO: add urls
     + "style=\"background: linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent), url(" + app.header + "), #FFF; background-repeat: no-repeat; background-size: cover; background-position: center;\""
     + "><p class=\"info\"><b>" + app.name + "</b> - " + app.rating + "/10</p></div>";
 }
 
 function smallCardsMethod(app) {
-  return "<div class=\"smallcard\" onclick=\"location.href = \'" + "\';\">"
+  return "<div class=\"smallcard\" onclick=\"setPage(\'404\');\">"
     + "<img src=\"" + app.icon + "\">"
     + "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author + "</small><small style=\"float: right;\"><b>" + app.rating + "</b></small></p></div>";
 }
