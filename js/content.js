99d768e0f600d68df4f814e32b711c9441c4ab48
James Fenn <18jafenn90@gmail.com>
2017-11-21 19:31:46 -0500

    reorganized things

--- js/content.js
+++ js/content.js
@@ -1,44 +1,62 @@
-/**
- * Adds a set of featured elements to the element passed
- * using the method provided.
- */
+//this is all test data, it means nothing
+var categories = [
+  {
+      "name": "stuff",
+      "description": "A bunch of stuff.",
+      "color": "#000",
+      "background": "transparent"
+  },
+  {
+    "name": "apps",
+    "description": "A bunch of apps.",
+    "color": "#BDBDBD",
+    "background": "#303030"
+  },
+  {
+    "name": "games",
+    "description": "A bunch of games.",
+    "color": "#177323",
+    "background": "#7CF38B"
+  },
+  {
+    "name": "more stuff",
+    "description": "A bunch of more stuff with a long description.",
+    "color": "#000",
+    "background": "transparent"
+  }
+];
+
+var apps = [
+  {
+    "name": "Status",
+    "author": "James Fenn",
+    "rating": "8.9",
+    "package": "com.james.status",
+    "header": "https://raw.githubusercontent.com/TheAndroidMaster/TheAndroidMaster.github.io/master/images/headers/status_bg.png",
+    "icon": "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png"
+  },
+  {
+    "name": "Metronome",
+    "author": "James Fenn",
+    "rating": 9.2,
+    "package": "james.metronome",
+    "header": "https://theandroidmaster.github.io/images/headers/metronomePremium.png",
+    "icon": "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png"
+  }
+];
+
 function getFeatured(element, method) {
-  element.innerHTML = method("Status", "James Fenn", "8.9/10", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/TheAndroidMaster.github.io/master/images/headers/status_bg.png")
-	  + method("Metronome", "James Fenn", "9.2/10", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/TheAndroidMaster.github.io/master/images/headers/metronomePremium.png")
-		+ method("Status", "James Fenn", "8.9/10", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/TheAndroidMaster.github.io/master/images/headers/status_bg.png")
-	  + method("Metronome", "James Fenn", "9.2/10", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/TheAndroidMaster.github.io/master/images/headers/metronomePremium.png")
+  element.innerHTML = "";
+  for (var i = 0; i < apps.length; i++) {
+    element.innerHTML += method(apps[i]);
+  }
 }
 
 function getCategories(element, method) {
 	element.innerHTML = "";
-	var categories = [
-    {
-  			"name": "stuff",
-  			"description": "A bunch of stuff.",
-  			"color": "#000",
-  			"background": "transparent"
-  	},
-    {
-			"name": "apps",
-			"description": "A bunch of apps.",
-			"color": "#BDBDBD",
-			"background": "#303030"
-		},
-		{
-			"name": "games",
-			"description": "A bunch of games.",
-			"color": "#177323",
-			"background": "#7CF38B"
-		},
-    {
-			"name": "more stuff",
-			"description": "A bunch of more stuff with a long description.",
-			"color": "#000",
-			"background": "transparent"
-		}
-	];
-
+  console.log("called" + categories.length);
 	for (var i = 0; i < categories.length; i++) {
+    console.log(i);
 		element.innerHTML += "<div id=\"" + categories[i].name + "Category\"></div>";
 	}
 
@@ -50,19 +68,18 @@ function getCategories(element, method) {
 }
 
 function getCategory(category, method) {
-	document.getElementById(category.name + "Category").innerHTML = "<div class=\"category row\" style=\"color: " + category.color + "; background-color: " + category.background + ";\">"
+  var element = document.getElementById(category.name + "Category");
+	element.innerHTML = "<div class=\"category row\" style=\"color: " + category.color + "; background-color: " + category.background + ";\">"
 		+ "<div style=\"vertical-align: top; margin-top: 3.5em;\"><h1>" + category.name.charAt(0).toUpperCase() + category.name.substring(1) + "</h1><p>" + category.description + "</p><br><button class=\"outline\">MORE</button></div>"
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
-		+ method("Status", "James Fenn", "8.9", "https://github.com/TheAndroidMaster/Status", "https://raw.githubusercontent.com/TheAndroidMaster/Status/master/app/src/main/res/mipmap-xxxhdpi/ic_launcher_web.png")
-		+ method("Metronome", "James Fenn", "9.2", "https://github.com/TheAndroidMaster/Metronome", "https://raw.githubusercontent.com/TheAndroidMaster/Metronome-Android/master/app/src/main/res/mipmap/ic_launcher_web.png")
+		+ method(apps[0])
+    + method(apps[1])
+    + method(apps[0])
+    + method(apps[1])
+    + method(apps[0])
+    + method(apps[1])
+    + method(apps[0])
+    + method(apps[1])
+    + method(apps[0])
+    + method(apps[1])
 		+ "</div>";
 }
--- js/methods.js
+++ js/methods.js
@@ -1,15 +1,11 @@
-function getLargeCardsMethod() {
-  return function(name, author, rating, url, image) {
-    return "<div class=\"largecard\" onclick=\"location.href = \'" + url + "\';\" "
-    + (image ? "style=\"background: linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent), url(" + image + "), #FFF; background-repeat: no-repeat; background-size: cover; background-position: center;\"" : "")
-    + "><p class=\"info\"><b>" + name + "</b> - " + rating + "</p></div>"
-  };
+function largeCardsMethod(app) {
+  return "<div class=\"largecard\" onclick=\"location.href = \'" + "\';\" " //TODO: add urls
+    + "style=\"background: linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent), url(" + app.header + "), #FFF; background-repeat: no-repeat; background-size: cover; background-position: center;\""
+    + "><p class=\"info\"><b>" + app.name + "</b> - " + app.rating + "/10</p></div>";
 }
 
-function getSmallCardsMethod() {
-  return function(name, author, rating, url, image) {
-    return "<div class=\"smallcard\" onclick=\"location.href = \'" + url + "\';\">"
-    + (image ? "<img src=\"" + image + "\">" : "")
-    + "<p class=\"info\"><b>" + name + "</b><br><small style=\"position: absolute;\">" + author + "</small><small style=\"float: right;\"><b>" + rating + "</b></small></p></div>"
-  };
+function smallCardsMethod(app) {
+  return "<div class=\"smallcard\" onclick=\"location.href = \'" + "\';\">"
+    + "<img src=\"" + app.icon + "\">"
+    + "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author + "</small><small style=\"float: right;\"><b>" + app.rating + "</b></small></p></div>";
 }
