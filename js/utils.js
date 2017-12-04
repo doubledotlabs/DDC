8987e493ac67185c647d1785332b26991c3f8df2
TheAndroidMaster <18jafenn90@gmail.com>
2017-12-04 08:35:13 -0500

    IE compatibility sucks

--- js/utils.js
+++ js/utils.js
@@ -65,7 +65,7 @@ function stringToArguments(args) {
 	var array = args.split("&");
 	var args = {};
 	for (var i = 0; i < array.length; i++) {
-		if (array[i].includes("=")) {
+		if (array[i].indexOf("=") >= 0) {
 			var argArray = array[i].split("=");
 			args[argArray[0]] = argArray[1];
 		} else args[array[i]] = true;
