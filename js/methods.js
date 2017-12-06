6daa770784a2a0707a765898728ae4c8c9a5872d
TheAndroidMaster <18jafenn90@gmail.com>
2017-12-06 08:46:57 -0500

    WHY DOESN'T INTERNET EXPLORER SUPPORT JS CLASSES

--- js/methods.js
+++ js/methods.js
@@ -1,105 +1,92 @@
-class AppMethods {
-  static largeCard(app) {
-    return "<div class=\"largecard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\" "
-      + "style=\"background: linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent), url(" + app.header + "), #FFF; background-repeat: no-repeat; background-size: cover; background-position: center;\""
-      + "><p class=\"info\"><b>" + app.name + "</b>&nbsp;&ndash;&nbsp;" + app.rating + "/5</p></div>";
-  }
+var AppMethods = {};
+AppMethods.largeCard = function(app) {
+	return "<div class=\"largecard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\" "
+		+ "style=\"background: linear-gradient(to top, rgba(255, 255, 255, 0.9), transparent), url(" + app.header + "), #FFF; background-repeat: no-repeat; background-size: cover; background-position: center;\""
+		+ "><p class=\"info\"><b>" + app.name + "</b>&nbsp;&ndash;&nbsp;" + app.rating + "/5</p></div>";
+};
 
-  static smallCard(app) {
-    return "<div class=\"smallcard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
-      + "<img src=\"" + app.icon + "\">"
-      + "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author.name
-      + "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
-  }
+AppMethods.smallCard = function(app) {
+	return "<div class=\"smallcard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
+		+ "<img src=\"" + app.icon + "\">"
+		+ "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author.name
+		+ "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
+};
 
-  static tinyCard(app) {
-    return "<div class=\"tinycard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
-      + "<img src=\"" + app.icon + "\">"
-      + "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author.name
-      + "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
-  }
+AppMethods.tinyCard = function(app) {
+	return "<div class=\"tinycard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
+		+ "<img src=\"" + app.icon + "\">"
+		+ "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.author.name
+		+ "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
+};
 
-  static consoleTinyCard(app) {
-    return "<div class=\"tinycard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
-      + "<img src=\"" + app.icon + "\">"
-      + "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.downloads + " downloads"
-      + "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
-  }
-}
+AppMethods.consoleTinyCard = function(app) {
+	return "<div class=\"tinycard\" onclick=\"setPage(\'page=app&package=" + app.package + "\');\">"
+		+ "<img src=\"" + app.icon + "\">"
+		+ "<p class=\"info\"><b>" + app.name + "</b><br><small style=\"position: absolute;\">" + app.downloads + " downloads"
+		+ "</small><small style=\"float: right;\"><b style=\"color: " + SiteUtils.getRatingColor(app.rating) + ";\">" + app.rating + "&nbsp;&#9733;</b></small></p></div>";
+};
 
-class ImageMethods {
+var ImageMethods = {};
+ImageMethods.card = function(image) {
+	return "<img class=\"card\" src=\"" + image.url + "\" onclick=\"location.href=\'" + image.url + "\';\">";
+};
 
-  static card(image) {
-    return "<img class=\"card\" src=\"" + image.url + "\" onclick=\"location.href=\'" + image.url + "\';\">";
-  }
+var CategoryMethods = {};
+CategoryMethods.chip = function(category) {
+	return "<p class=\"chip\"" + (category.id ? " onclick=\"setPage(\'page=category&id=" + category.id + "\');\"" : "") + ">" + category.name + "</p>";
+};
 
-}
+var ReviewMethods = {};
+ReviewMethods.normal = function(data) {
+	return "<div class=\"review\"><div class=\"reviewinfo\" onclick=\"setPage(\'page=user&id=" + data.user.id + "\')\"><img src=\"" + data.user.image + "\"><div><b>" + data.user.name + "</b><br>"
+		+ ratingMethod(data.rating) + "</div></div>"
+		+ "<p><small>Reviewed " + (data.app ? "<a href=\"javascript:setPage(\'page=app&package=" + data.app.package + "\');\">" + data.app.name + "</a> " : "") + "on " + data.date + "</small></p>"
+		+ "<p>" + (data.review ? data.review + "</p>" : data.summary + "</p><a href=\"javascript:setPage(\'page=review&id=" + data.id + "\')\">Read More</a>")
+		+ "</div>";
+};
 
-class CategoryMethods {
+ReviewMethods.console = function(data) {
+	return "<div class=\"review\"><div class=\"reviewinfo\"><img src=\"" + data.user.image + "\"><div><b>" + data.user.name + "</b><br>"
+		+ ratingMethod(data.rating) + "</div></div>"
+		+ "<p><small>" + data.date + "</small></p>"
+		+ "<p>" + (data.review ? data.review + "</p><div class=\"input long\" contentEditable=\"true\" placeholder=\"Reply to review...\"></div><button class=\"outline\">REPLY</button>" : data.summary
+		+ "</p><a href=\"javascript:setPage(\'page=review&id=" + data.id + "\')\">Read More / Reply</a>")
+		+ "</div>";
+};
 
-  static chip(data) {
-    return "<p class=\"chip\"" + (data.id ? " onclick=\"setPage(\'page=category&id=" + data.id + "\');\"" : "") + ">" + data.name + "</p>";
-  }
+var DownloadMethods = {};
+DownloadMethods.normal = function(data) {
+	var dpi = null;
+	if (data.download.minDpi && data.download.maxDpi)
+		dpi = data.download.minDpi + " - " + data.download.maxDpi + "dpi";
+	else if (data.download.minDpi)
+		dpi = data.download.minDpi + "dpi+";
+	else if (data.download.maxDpi)
+		dpi = "0 - " + data.download.maxDpi + "dpi";
 
-}
-
-class ReviewMethods {
-
-  static normal(data) {
-    return "<div class=\"review\"><div class=\"reviewinfo\" onclick=\"setPage(\'page=user&id=" + data.user.id + "\')\"><img src=\"" + data.user.image + "\"><div><b>" + data.user.name + "</b><br>"
-      + ratingMethod(data.rating) + "</div></div>"
-      + "<p><small>Reviewed " + (data.app ? "<a href=\"javascript:setPage(\'page=app&package=" + data.app.package + "\');\">" + data.app.name + "</a> " : "") + "on " + data.date + "</small></p>"
-      + "<p>" + (data.review ? data.review + "</p>" : data.summary + "</p><a href=\"javascript:setPage(\'page=review&id=" + data.id + "\')\">Read More</a>")
-      + "</div>"
-  }
-
-  static console(data) {
-    return "<div class=\"review\"><div class=\"reviewinfo\"><img src=\"" + data.user.image + "\"><div><b>" + data.user.name + "</b><br>"
-      + ratingMethod(data.rating) + "</div></div>"
-      + "<p><small>" + data.date + "</small></p>"
-      + "<p>" + (data.review ? data.review + "</p><div class=\"input long\" contentEditable=\"true\" placeholder=\"Reply to review...\"></div><button class=\"outline\">REPLY</button>" : data.summary 
-      + "</p><a href=\"javascript:setPage(\'page=review&id=" + data.id + "\')\">Read More / Reply</a>")
-      + "</div>"
-  }
-
-}
-
-class DownloadMethods {
-
-  static normal(data) {
-  	var dpi = null;
-  	if (data.download.minDpi && data.download.maxDpi)
-  		dpi = data.download.minDpi + " - " + data.download.maxDpi + "dpi";
-  	else if (data.download.minDpi)
-  		dpi = data.download.minDpi + "dpi+";
-  	else if (data.download.maxDpi)
-  		dpi = "0 - " + data.download.maxDpi + "dpi";
-
-    return "<div class=\"download\">"
-      + "<div class=\"downloadheader\">"
-      + "<span>" + data.release.date + "</span>"
-      + "<i class=\"material-icons downloadicon\" onclick=\"location.href=\'download:" + data.download.url + "\';\">file_download</i>"
-      + "</div><hr>"
-      + "<div class=\"releasecontent\">"
-      + "<p><b>Version " + data.release.version + "</b></p>"
-      + data.release.notes
-      + "</div>"
-      + "<div id=\"downloadContent" + data.download.url + "\" class=\"downloadinfo content\">"
-  		+ "<i class=\"expand material-icons\" onclick=\"document.getElementById(\'downloadContent" + data.download.url + "\').classList.remove(\'active\');\">expand_more</i><hr>"
-      + "<span><b>Target SDK:</b> Android " + AndroidUtils.sdkToVersion(data.download.target) + "</span>"
-      + "<span><b>Required SDK:</b> Android " + AndroidUtils.sdkToVersion(data.download.min) + (data.download.max ? " - " + AndroidUtils.sdkToVersion(data.download.max) : "+") + "</span>"
-      + (data.download.config ? "<span><b>Configuration:</b> " + data.download.config + "</span>" : "")
-  		+ (data.download.arch ? "<span><b>Architecture:</b> " + data.download.arch + "</span>" : "")
-  		+ (dpi ? "<span><b>Screen Scale:</b> " + dpi + "</span>" : "")
-      + "<span><b>Download Size:</b> " + data.download.size + "</span>"
-      + "</div>"
-      + "<div class=\"expand\" onclick=\"document.getElementById(\'downloadContent" + data.download.url + "\').classList.add(\'active\');\">"
-      + "<hr><div class=\"stat\"><i class=\"material-icons\">info</i><span>APK INFO</span></div>"
-      + "</div>"
-      + "</div>";
-  }
-
-}
+	return "<div class=\"download\">"
+		+ "<div class=\"downloadheader\">"
+		+ "<span>" + data.release.date + "</span>"
+		+ "<i class=\"material-icons downloadicon\" onclick=\"location.href=\'download:" + data.download.url + "\';\">file_download</i>"
+		+ "</div><hr>"
+		+ "<div class=\"releasecontent\">"
+		+ "<p><b>Version " + data.release.version + "</b></p>"
+		+ data.release.notes
+		+ "</div>"
+		+ "<div id=\"downloadContent" + data.download.url + "\" class=\"downloadinfo content\">"
+		+ "<i class=\"expand material-icons\" onclick=\"document.getElementById(\'downloadContent" + data.download.url + "\').classList.remove(\'active\');\">expand_more</i><hr>"
+		+ "<span><b>Target SDK:</b> Android " + AndroidUtils.sdkToVersion(data.download.target) + "</span>"
+		+ "<span><b>Required SDK:</b> Android " + AndroidUtils.sdkToVersion(data.download.min) + (data.download.max ? " - " + AndroidUtils.sdkToVersion(data.download.max) : "+") + "</span>"
+		+ (data.download.config ? "<span><b>Configuration:</b> " + data.download.config + "</span>" : "")
+		+ (data.download.arch ? "<span><b>Architecture:</b> " + data.download.arch + "</span>" : "")
+		+ (dpi ? "<span><b>Screen Scale:</b> " + dpi + "</span>" : "")
+		+ "<span><b>Download Size:</b> " + data.download.size + "</span>"
+		+ "</div>"
+		+ "<div class=\"expand\" onclick=\"document.getElementById(\'downloadContent" + data.download.url + "\').classList.add(\'active\');\">"
+		+ "<hr><div class=\"stat\"><i class=\"material-icons\">info</i><span>APK INFO</span></div>"
+		+ "</div>"
+		+ "</div>";
+};
 
 function linksMethod(link) {
   var icon = "link";
@@ -135,63 +122,60 @@ function ratingMethod(rating, color) {
   return html + "</span>";
 }
 
-class ListMethods {
+var ListMethods = {};
+ListMethods.none = function(category, apps, method) {
+	var html = "";
+	for (var i = 0; i < apps.length; i++) {
+		html += method(apps[i]);
+	}
+	return html;
+};
 
-  static none(category, apps, method) {
-  	var html = "";
-  	for (var i = 0; i < apps.length; i++) {
-  		html += method(apps[i]);
-  	}
-  	return html;
-  }
+ListMethods.carousel = function(options, apps, method) {
+	var html = "<div class=\"carousel\" data-slick=\'" + JSON.stringify(options) + "\'>";
 
-  static carousel(options, apps, method) {
-    var html = "<div class=\"carousel\" data-slick=\'" + JSON.stringify(options) + "\'>";
+	for (var i = 0; i < apps.length; i++) {
+		html += method(apps[i]);
+	}
 
-    for (var i = 0; i < apps.length; i++) {
-  		html += method(apps[i]);
-  	}
+	return html + "</div>";
+};
 
-    return html + "</div>";
-  }
+ListMethods.row = function(category, apps, method) {
+	var html = (category.id ? "" : "<h1 class=\"indented\">" + category.name + "</h1>") + "<div class=\"" + (category.description ? "category " : "") + "row\""
+		+ (category.color && category.background ? " style=\"position: relative; color: " + category.color + "; background-color: " + category.background + ";\">" : " style=\"position: relative;\">")
+		+ (category.id && category.name ? "<div class=\"rowinfo\" style=\"vertical-align: top; transform: translateY(calc(4em - 20%));\"><h2>" + category.name + "</h2>" + (category.description ? "<p>" + category.description
+		+ "</p>" : "") + "<button class=\"outline\" onclick=\"setPage(\'page=category&id=" + category.id + "\')\">MORE</button></div>" : "");
 
-  static row(category, apps, method) {
-  	var html = (category.id ? "" : "<h1 class=\"indented\">" + category.name + "</h1>") + "<div class=\"" + (category.description ? "category " : "") + "row\""
-  		+ (category.color && category.background ? " style=\"position: relative; color: " + category.color + "; background-color: " + category.background + ";\">" : " style=\"position: relative;\">")
-      + (category.id && category.name ? "<div class=\"rowinfo\" style=\"vertical-align: top; transform: translateY(calc(4em - 20%));\"><h2>" + category.name + "</h2>" + (category.description ? "<p>" + category.description
-  		+ "</p>" : "") + "<button class=\"outline\" onclick=\"setPage(\'page=category&id=" + category.id + "\')\">MORE</button></div>" : "");
+	for (var i = 0; i < apps.length; i++) {
+		html += method(apps[i]);
+	}
 
-  	for (var i = 0; i < apps.length; i++) {
-  		html += method(apps[i]);
-  	}
+	return html + "<div class=\"fadeoverlay\" style=\"background: linear-gradient(to right, transparent, "
+		+ (category.shade ? category.shade : (category.background && category.background != "transparent" ? category.background : "#FFF")) + ");\"></div></div>";
+};
 
-  	return html + "<div class=\"fadeoverlay\" style=\"background: linear-gradient(to right, transparent, "
-      + (category.shade ? category.shade : (category.background && category.background != "transparent" ? category.background : "#FFF")) + ");\"></div></div>";
-  }
+ListMethods.overflow = function(category, apps, method) {
+	var html = (category.name ? "<div class=\"category\">"
+		+ "<div style=\"margin: 2em 0;\"><h1" + (category.color ? " style=\"color: " + category.color + ";\"" : "") + ">" + category.name + "</h1>"
+		+ (category.description ? "<p" + (category.color ? " style=\"color: " + category.color + ";\"" : "") + ">" + category.description + "</p>" : "") + "</div>" : "")
+		+ "<div class=\"row overflow\">";
 
-  static overflow(category, apps, method) {
-  	var html = (category.name ? "<div class=\"category\">"
-  	  + "<div style=\"margin: 2em 0;\"><h1" + (category.color ? " style=\"color: " + category.color + ";\"" : "") + ">" + category.name + "</h1>"
-      + (category.description ? "<p" + (category.color ? " style=\"color: " + category.color + ";\"" : "") + ">" + category.description + "</p>" : "") + "</div>" : "")
-      + "<div class=\"row overflow\">";
+	for (var i = 0; i < apps.length; i++) {
+		html += method(apps[i]);
+	}
 
-  	for (var i = 0; i < apps.length; i++) {
-  		html += method(apps[i]);
-  	}
+	if (category.description)
+		html += "</div>";
+	return html + "</div>";
+};
 
-  	if (category.description)
-      html += "</div>";
-  	return html + "</div>";
-  }
+ListMethods.grid = function(category, apps, method) {
+	var html = (category.name ? "<h1 class=\"indented\">" + category.name + "</h1>" : "") + "<div class=\"row grid\">";
 
-  static grid(category, apps, method) {
-    var html = (category.name ? "<h1 class=\"indented\">" + category.name + "</h1>" : "") + "<div class=\"row grid\">";
+	for (var i = 0; i < apps.length; i++) {
+		html += method(apps[i]);
+	}
 
-    for (var i = 0; i < apps.length; i++) {
-  		html += method(apps[i]);
-  	}
-
-    return html + "</div>";
-  }
-
-}
+	return html + "</div>";
+};
