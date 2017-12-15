2fd7333dd66bc0d2ac46d4fe4a8eb455a365bcc5
TheAndroidMaster <18jafenn90@gmail.com>
2017-12-15 09:32:52 -0500

    added description editing

---
+++ console/js/content.js
@@ -0,0 +1,24 @@
+function setAppPage(id, name, summary, links, fun, ignore) {
+	firebase.database().ref("apps/" + id.split(".").join("_")).update({
+		"name": name,
+		"summary": summary,
+		"links": links
+	}).then(fun, function(error) {
+		console.log("Couldn\'t update app page, " + error);
+		if (ignore) {
+			fun();
+		} else {
+			setPage("page=404", true);
+		}
+	});
+}
+
+function setAppDescription(id, description, fun, ignore) {
+	firebase.database().ref("apps/" + id.split(".").join("_") + "/description").set(description).then(fun, function(error) {
+		if (ignore) {
+			fun();
+		} else {
+			setPage("page=404", true);
+		}
+	})
+}
